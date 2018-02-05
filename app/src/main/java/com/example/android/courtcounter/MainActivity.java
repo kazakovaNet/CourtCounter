e56ec328277e70c555fa642752306db856a6d73f
kazakovaNet <kazakova.net@yandex.ru>
2018-02-06 00:16:17 +0300

    Implemented state preservation (in progress)

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,22 +1,54 @@
 package com.example.android.courtcounter;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String GOAL_TEAM_A = "goalTeamA";
+    public static final String GOAL_TEAM_B = "goalTeamB";
+    public static final String FREE_KICK_TEAM_A = "freeKickTeamA";
+    public static final String FREE_KICK_TEAM_B = "freeKickTeamB";
     int goalTeamA = 0;
     int goalTeamB = 0;
     int freeKickTeamA = 0;
     int freeKickTeamB = 0;
+    private TextView mGoalTeamATextView;
+    private TextView mFreeKickTeamATextView;
+    private TextView mGoalTeamBTextView;
+    private TextView mFreeKickTeamBTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null && savedInstanceState.containsKey(GOAL_TEAM_A)) {
+            goalTeamA = savedInstanceState.getInt(GOAL_TEAM_A);
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey(GOAL_TEAM_B)) {
+            goalTeamB = savedInstanceState.getInt(GOAL_TEAM_B);
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey(FREE_KICK_TEAM_A)) {
+            freeKickTeamA = savedInstanceState.getInt(FREE_KICK_TEAM_A);
+        }
+        if (savedInstanceState != null && savedInstanceState.containsKey(FREE_KICK_TEAM_B)) {
+            freeKickTeamB = savedInstanceState.getInt(FREE_KICK_TEAM_B);
+        }
+
         displayScore();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+
+        outState.putInt(GOAL_TEAM_A, Integer.parseInt(String.valueOf(mGoalTeamATextView.getText())));
+        outState.putInt(GOAL_TEAM_B, Integer.parseInt(String.valueOf(mGoalTeamBTextView.getText())));
+        outState.putInt(FREE_KICK_TEAM_A, Integer.parseInt(String.valueOf(mFreeKickTeamATextView.getText())));
+        outState.putInt(FREE_KICK_TEAM_B, Integer.parseInt(String.valueOf(mFreeKickTeamBTextView.getText())));
     }
 
     /**
@@ -69,32 +101,32 @@ public class MainActivity extends AppCompatActivity {
      * Displays the given goals for Team A.
      */
     public void displayGoalForTeamA() {
-        TextView goalTeamATextView = findViewById(R.id.team_a_goal);
-        goalTeamATextView.setText(String.valueOf(goalTeamA));
+        mGoalTeamATextView = findViewById(R.id.team_a_goal);
+        mGoalTeamATextView.setText(String.valueOf(goalTeamA));
     }
 
     /**
      * Displays the given free kicks for Team A.
      */
     private void displayFreeKickForTeamA() {
-        TextView freeKickTeamATextView = findViewById(R.id.team_a_free_kick);
-        freeKickTeamATextView.setText(String.valueOf(freeKickTeamA));
+        mFreeKickTeamATextView = findViewById(R.id.team_a_free_kick);
+        mFreeKickTeamATextView.setText(String.valueOf(freeKickTeamA));
     }
 
     /**
      * Displays the given goals for Team B.
      */
     public void displayGoalForTeamB() {
-        TextView goalTeamBTextView = findViewById(R.id.team_b_goal);
-        goalTeamBTextView.setText(String.valueOf(goalTeamB));
+        mGoalTeamBTextView = findViewById(R.id.team_b_goal);
+        mGoalTeamBTextView.setText(String.valueOf(goalTeamB));
     }
 
     /**
      * Displays the given free kicks for Team B.
      */
     private void displayFreeKickForTeamB() {
-        TextView FreeKickTeamBTextView = findViewById(R.id.team_b_free_kick);
-        FreeKickTeamBTextView.setText(String.valueOf(freeKickTeamB));
+        mFreeKickTeamBTextView = findViewById(R.id.team_b_free_kick);
+        mFreeKickTeamBTextView.setText(String.valueOf(freeKickTeamB));
     }
 
     /**
