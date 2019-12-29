494a4aebdfe0a7e514455a70708c2dc1a777e679
KazakovaNet <kazakovaNet@yandex.ru>
2019-12-29 20:17:26 +0300

    Update gradle and libraries versions

--- app/src/main/java/com/example/android/courtcounter/activity/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/activity/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.android.courtcounter.activity;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.courtcounter.R;
 
